38dcbbfca78ed4358f8b014f0ddf79bfd0e53ec2
Roman Castro <xxromanxxtm@gmail.com>
2025-07-29 02:58:51 -0300

    scroll

--- app/sections/FirstSection.jsx
+++ app/sections/FirstSection.jsx
@@ -7,7 +7,7 @@ export const FirstSection = () => {
             <h1 id="firstSectionTitle">CASA DE DIOS</h1>
             <p id="firstSectionP">SIEMPRE SOS</p>
             <h2 id="firstSectionH2">Bienvenido</h2>
-            <a href="#" id="firstSectionA">MÁS INFORMACIÓN</a>
+            <a href="#secondSection" id="firstSectionA">MÁS INFORMACIÓN</a>
             <Image id="firstSectionImage" src="/images/firstSection.jpg" alt="First Section Image" width={1280} height={720} />
         </section>
     );
