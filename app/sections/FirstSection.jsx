74aa230af733f9c904c29d31cf082b253ecc372c
Roman Castro <xxromanxxtm@gmail.com>
2025-07-29 02:31:36 -0300

    first section actualizacion

--- app/sections/FirstSection.jsx
+++ app/sections/FirstSection.jsx
@@ -3,10 +3,12 @@ import Image from "next/image";
 export const FirstSection = () => {
     return (
         <section id="firstSection">
-            <Image id="firstSectionImage" src="/logo.png" alt="First Section Logo" width={500} height={500} />
+            <Image id="firstSectionLogo" src="/logo.png" alt="First Section Logo" width={500} height={500} />
             <h1 id="firstSectionTitle">CASA DE DIOS</h1>
             <p id="firstSectionP">SIEMPRE SOS</p>
             <h2 id="firstSectionH2">Bienvenido</h2>
+            <a href="#" id="firstSectionA">MÁS INFORMACIÓN</a>
+            <Image id="firstSectionImage" src="/images/firstSection.jpg" alt="First Section Image" width={1280} height={720} />
         </section>
     );
 };
